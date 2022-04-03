8a1c5d45dd338b1441d91aac6e67a674110f01f9
Marcin <marcin.zdradzisz@ezse.pl>
2022-04-03 13:17:29 +0200

    Search

--- src/views/List/List.js
+++ src/views/List/List.js
@@ -1,25 +1,46 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
-import {ListGroup} from "react-bootstrap";
+import {Button, Form, FormControl, InputGroup, ListGroup} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
 import axios from "axios";
 
 
-
 const List = () => {
-const [adverts, setAdverts] = useState([]);
-useEffect( ()=>{
-    const fetchData = async()=>{
-        const response = await axios.get('/adverts');
+    const [adverts, setAdverts] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('/adverts');
+            setAdverts(response.data);
+        }
+        fetchData();
+    }, []);
+    const [search, setSearch] = useState("");
+    const handleSearchChange = (event) => {
+        setSearch(event.target.value);
+    };
+    const handleFormSubmit = async (event) =>{
+        event.preventDefault();
+        // const response = await axios.get(`/adverts?q=${search}`);
+        const response = await axios.get(`/adverts`, {params: {q: search}});
         setAdverts(response.data);
     }
-    fetchData();
-},[])
+
     return (
         <div>
             <h1>List</h1>
-            <Link to={"/advert"}>advert</Link>
+            <Form onSubmit={handleFormSubmit}>
 
+                <InputGroup className="mb-3">
+                    <FormControl
+                        placeholder="Search for"
+                        value={search}
+                        onChange={handleSearchChange}
+                    />
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </InputGroup>
+            </Form>
             <ListGroup>
                 {adverts.map(advert => (
                     <LinkContainer key={advert.id} to={`/advert/${advert.id}`}>
