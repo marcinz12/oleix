3200a67b41ca297e96b166a2df6dcf3175c90261
Marcin <marcin.zdradzisz@ezse.pl>
2022-04-03 12:46:52 +0200

    list and advert

--- src/views/Detail/Detail.js
+++ src/views/Detail/Detail.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, useParams} from "react-router-dom";
+import axios from "axios";
+import {Badge, Card, Col, Container, Image, Row, Stack} from "react-bootstrap";
 
 const Detail = () => {
-   const params = useParams();
+    const params = useParams();
+    const [advert, setAdvert] = useState({});
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(`/adverts/${params.id}`);
+            setAdvert(response.data);
+        }
+        fetchData();
+    }, [params.id])
     return (
         <div>
             <h1>Detail nr. {params.id}</h1>
+            <h2>Title: {advert.title}</h2>
+            <Row>
+                <Col xs={12} md={"auto"}>
+                    <Image src={advert.image}/>
+                </Col>
+                <Col>
+                    <Card body>
+                        <Row className={"mb-3"}>
+                            <Col>
+                                {advert.seller}<br/>
+                                {advert.sellerPhone}
+                            </Col>
+                            <Col>
+                                <Stack>
+                                    <h3 className="text-end"> ${advert.price}</h3>
+                                    {advert.canNegotiate && (<Badge bg={"primary"}>Can negotiate</Badge>)}
+                                </Stack>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <p>
+                                    {advert.description}
+                                </p>
+                            </Col>
+                        </Row>
+                    </Card>
+
+                </Col>
+            </Row>
             <Link to={"/"} className="btn btn-outline-secondary">to list</Link>
             <Link to={-1}>back</Link>
         </div>
--- src/views/List/List.js
+++ src/views/List/List.js
@@ -1,60 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import {ListGroup} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
+import axios from "axios";
 
-const adverts = [
-    {
-        "id": 0,
-        "title": "Fantastic Fresh Shoes",
-        "price": "141.00",
-        "description": "The Football Is Good For Training And Recreational Purposes",
-        "seller": "Lexus",
-        "image": "http://placeimg.com/400/400/business",
-        "sellerPhone": "+48 950 865 197",
-        "canNegotiate": true,
-        "createdOn": "2022-03-18T15:26:36.993Z",
-        "categoryId": 4
-    },
-    {
-        "id": 1,
-        "title": "Small Cotton Car",
-        "price": "976.00",
-        "description": "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
-        "seller": "Jonathan",
-        "image": "http://placeimg.com/400/400/business",
-        "sellerPhone": "+48 942 306 474",
-        "canNegotiate": false,
-        "createdOn": "2022-03-17T22:48:14.040Z",
-        "categoryId": 9
-    },
-    {
-        "id": 2,
-        "title": "Practical Cotton Chair",
-        "price": "43.00",
-        "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
-        "seller": "Adah",
-        "image": "http://placeimg.com/400/400/business",
-        "sellerPhone": "+48 287 383 294",
-        "canNegotiate": true,
-        "createdOn": "2022-03-18T16:31:55.466Z",
-        "categoryId": 7
-    },
-    {
-        "id": 3,
-        "title": "Rustic Fresh Chips",
-        "price": "847.00",
-        "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
-        "seller": "Sean",
-        "image": "http://placeimg.com/400/400/business",
-        "sellerPhone": "+48 745 549 144",
-        "canNegotiate": true,
-        "createdOn": "2022-03-18T00:19:12.261Z",
-        "categoryId": 5
-    },
-]
+
 
 const List = () => {
+const [adverts, setAdverts] = useState([]);
+useEffect( ()=>{
+    const fetchData = async()=>{
+        const response = await axios.get('/adverts');
+        setAdverts(response.data);
+    }
+    fetchData();
+},[])
     return (
         <div>
             <h1>List</h1>
